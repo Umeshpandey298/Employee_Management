0654b6677dc9a1827427c36071f24500602dc889
umesh <umeshpandey298@gmail.com>
2024-12-15 16:47:36 -0800

    .

--- emp/wsgi.py
+++ emp/wsgi.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from employee import app as application
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emp.settings')
 
